c4927c78bdd6300fdcabe73e8eb7c1c106b69d46
Nikita Titov <nekit94-08@mail.ru>
2020-09-15 09:25:34 +0300

    Update utils.py

--- tests/utils.py
+++ tests/utils.py
@@ -128,7 +128,7 @@ class ModelTrainer:
 
         (self.X_train, self.X_test,
          self.y_train, _) = train_test_split(
-            self.X, self.y, test_size=test_fraction, random_state=42)
+            self.X, self.y, test_size=test_fraction, random_state=14)
         if additional_test_data is not None:
             self.X_test = np.vstack((additional_test_data, self.X_test))
 
